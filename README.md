3a8becc247b654da277b4e68a83419347ef0eb7e
kylekizirian <kyle.kizirian@gmail.com>
2018-04-19 21:01:05 -0400

    Added function to generate random share of given length, added TODOs to README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # xor-secret-sharing
 
 Python application that generates deceptive secrets using an XOR-based scheme
+
+To run example:
+python ./xor_secret_sharing.py --secret "hello world" -- n 20 --m 5 --output ./output.txt
+
+TODO:
+Generate m fake secrets -> use free online text to generate real-looking sentences with same length as secret string?
+
+Generate shares for fake secrets based on given approach (NAIVE, SIMPLE, CYCLIC) 
--- xor_secret_sharing.py
+++ xor_secret_sharing.py
@@ -1,4 +1,11 @@
 import argparse
+import random
+
+def generate_random_share(length):
+    share = []
+    for _ in range(length):
+        share.append(random.randint(0, 127)) # ascii char/1 byte range
+    return share
 
 def generate_naive_secret_share():
     # TODO
@@ -25,6 +32,41 @@ if __name__ == '__main__':
     secret_approach.add_argument('--CYCLIC', help="CYCLIC approach to deceptive secret sharing", action='store_true')
 
     args = parser.parse_args()
+
+    print("Original secret:")
+    print(args.secret + "\n")
+
+    # generate n-1 random secret shares that are same length as secret
+    shares_list = []
+    for _ in range(int(args.n) - 1):
+        individual_share = generate_random_share(len(args.secret))
+        shares_list.append(individual_share)
+
+    secret_to_ascii = []
+    for i in range(len(args.secret)):
+        secret_to_ascii.append(ord(args.secret[i]))
+
+    # generate the last share by XOR each random share with original secret
+    last_share = []
+    for i in range(len(args.secret)):
+        val = 127
+        for j in range(len(shares_list)):
+            val = val ^ shares_list[j][i]
+        val = val ^ secret_to_ascii[i]
+        last_share.append(val)
+
+    shares_list.append(last_share)
+    recreated_secret = ""
+
+    # recreate original secret by XOR all shares together
+    for i in range(len(args.secret)):
+        val = 127
+        for j in range(len(shares_list)):
+            val = val ^ shares_list[j][i]
+        recreated_secret = recreated_secret + chr(val)
+    
+    print("Recreated secret:")
+    print(recreated_secret)
 
     if args.NAIVE:
       generate_naive_secret_share()
